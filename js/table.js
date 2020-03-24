9c05f680d41f6200f8ad47f0a7c76c45ca486d1e
NomalButHappy <271370166@qq.com>
2020-03-24 18:28:22 +0800

    pop页面表格再尝试

--- js/table.js
+++ js/table.js
@@ -1,17 +1,38 @@
 $(function(){
-	$.getJSON("data/table.json", function(json) {
-		console.log(json); // this will show the info it in firebug console
-	});
-	var str=[{name:"zy",age:111,sex:0,pass:1},{name:"zay",age:112},{name:"zby",age:113},{name:"zcy",age:114}];
+	var str=[
+	{Mon:"计算机图形学", Tue:"机器学习" , Wed:"", Thu:"上个吉儿课", Fri:"", Sat:"", Sun:""},
+	{Mon:"计算机图形学", Tue:"机器学习" , Wed:"", Thu:"上个吉儿课", Fri:"", Sat:"", Sun:""},
+	{Mon:"计算机图形学", Tue:"机器学习" , Wed:"", Thu:"上个吉儿课", Fri:"", Sat:"", Sun:""},
+	{Mon:"计算机图形学", Tue:"机器学习" , Wed:"", Thu:"上个吉儿课", Fri:"", Sat:"", Sun:""},
+	{Mon:"计算机图形学", Tue:"机器学习" , Wed:"", Thu:"上个吉儿课", Fri:"", Sat:"", Sun:""}
+	];
 	obj=eval(str);
-	  var ht = '';
+	  var ht = '<thead><tr><th>Table table</th><th>Mon.</th><th>Tue.</th><th>Wed.</th><th>Thu.</th><th>Fri.</th><th>Sat.</th><th>Sun.</th></tr></thead><tbody>';
 	  for(var i=0;i<obj.length;i++){
 	    ht = ht+'<tr>';
-	    ht = ht + '<td>' + obj[i].name + '</td>';
-	    ht = ht + '<td>' + obj[i].age + '</td>';
-	    ht = ht + '<td>' + obj[i].sex + '</td>';
-	    ht = ht + '<td>' + obj[i].pass + '</td>';
+		ht = ht + '<td>' + String(2*i+1) + '~' + String(2*i + 2) + '</td>';
+	    ht = ht + '<td>' + obj[i].Mon + '</td>';
+	    ht = ht + '<td>' + obj[i].Tue + '</td>';
+	    ht = ht + '<td>' + obj[i].Wed + '</td>';
+	    ht = ht + '<td>' + obj[i].Thu + '</td>';
+		ht = ht + '<td>' + obj[i].Fri + '</td>';
+		ht = ht + '<td>' + obj[i].Sat + '</td>';
+		ht = ht + '<td>' + obj[i].Sun + '</td>';
 	    ht = ht+'</tr>';
 	  }
+	  ht = ht + '</tbody>'
 	  $('#tb').append(ht);
 })
+
+
+function load(name) {
+    let xhr = new XMLHttpRequest(),
+        okStatus = document.location.protocol === "file:" ? 0 : 200;
+    xhr.open('GET', name, false);
+    xhr.overrideMimeType("text/html;charset=utf-8");//默认为utf-8
+    xhr.send(null);
+    return xhr.status === okStatus ? xhr.responseText : null;
+}
+ 
+let text = load("data/data.txt");
+console.log(text);
