e99cd550ba2750d0958cf50c08e223e13b9c24dc
Cocoon <3075861623@qq.com>
2020-11-30 03:16:59 +0800

    现在后台爬取可以运作

--- js/background.js
+++ js/background.js
@@ -34,6 +34,8 @@ function openUrlNewTab(url)
 //发送文本通知
 function notify(title, message, iconurl)
 {
+	console.log(title);
+	console.log(message);
 	if(!iconurl)
 		iconurl = icon;
     chrome.notifications.create(null, {
@@ -133,9 +135,17 @@ NotifyServer.start = function(interval){
 	// 此外，监听刚开始时理论上会进行一大波通知收录，届时会用到多条消息获取
 	// 每次监听后都会更新最近更新时间。获取通知时只会获取在上次更新时间后出现的通知
 	// 最初开始监听获得七天以内的消息
-	this.listener = setInterval(chrome.storage.sync.get({lasttime: new Date(new Date()-86400000*7)}, function(items){
-		lasttime = items['lasttime'];
-		getmessages = [];
+	var resettime = true;
+	var days = 21;
+	NotifyServer.listener = setInterval(()=>{chrome.storage.sync.get({lasttime: new Date(new Date()-86400000*days).toString()}, function(items){
+		if(resettime)
+		{
+			lasttime = new Date(new Date()-86400000*days);
+			resettime = false;
+		}
+		else
+			lasttime = new Date(items.lasttime);
+		console.log(lasttime); // star
 		//获取的同时检查消息是否有重复
 		function check(messages){
 			//过滤掉重复的消息并返回新的消息列表
@@ -156,14 +166,26 @@ NotifyServer.start = function(interval){
 		//整理未重复消息，按种类播送
 		//暂不支持图片消息
 		//同步执行异步操作效率很差，后面需要优化
-		for(i in this.targets)
+		for(i in NotifyServer.targets)
 		{
-			i.get(lasttime,function(tarmsgs){
+			NotifyServer.targets[i].get(lasttime,function(tarmsgs){
 				tarmsgs=check(tarmsgs);
-				getmessages.push.apply(getmessages,tarmsgs);
+				NotifyServer.messages.unshift.apply(NotifyServer.messages,tarmsgs);
+				if(tarmsgs.length>1)
+				{
+					title = "您有多条消息";
+					msglist=[];
+					tarmsgs.map((e,index)=>{message:msglist.push({title:NotifyServer.targets[i].name,message:e.message})});
+					console.log(msglist);
+					notify_many(title, msglist, icon);
+				}
+				else if(tarmsgs.length==1)
+					notify(NotifyServer.targets[i].name,tarmsgs[0].message);
 			});
 		}
+		/*
 		getmessages.sort(function(a,b){if(a.time>b.time) return 1; else return 0;}); //对消息按时间进行排序
+		console.log(getmessages);
 		if(getmessages.length>1)
 		{
 			title = "您有多条消息";
@@ -172,20 +194,22 @@ NotifyServer.start = function(interval){
 		else if(getmessages.length==1)
 			notify(getmessages[0]);
 		NotifyServer.messages.unshift.apply(NotifyServer.messages,getmessages);
-		chrome.storage.sync.set({lasttime:new Date()},function(){console.log('Catch messages at: '+lasttime.toString())}); //更新时间
-	}), interval);
-	return this.listener;
+		*/
+		chrome.storage.sync.set({lasttime:new Date().toString()},function(items){console.log('Catch messages at: '+new Date().toString())}); //更新时间
+	})}, interval);
+	return NotifyServer.listener;
 };
 
 NotifyServer.stop = function(){
-	clearInterval(this.listener);
-	this.listener = null;
+	clearInterval(NotifyServer.listener);
+	NotifyServer.listener = null;
 };
 NotifyServer.targets=[];
 
 var target={
 	name:'', //自定义名称
 	tag:'',  //通知来源，如"中南大学信息学院"之类， 因为对不同通知的获取程序需要分别设计，因此要加以区分
+	root_url:'',  //根路径，用于保证添加的路径与队友的来源相同
 	url:'',  //通知路径，监听将对具体路径内的消息列表进行监听
 	get:function(lasttime){},  //获取通知的具体方法，需要具体实现。该方法的返回值需要包括是否有新通知，每个新通知的标题以及可能的图片等
 					           //参数为上次获取时间。返回一个message列表，message内容见上
@@ -193,10 +217,11 @@ var target={
 	id='';
 	passwd=''
 	*/
-	newtag:function(tag, getf){
+	newtag:function(tag, root_url, getf){
 		//用于产生一条新的监听类型
 		res = this;
 		res.tag = tag;
+		res.root_url = root_url;
 		res.get = getf;
 		return res;
 	},
@@ -204,20 +229,22 @@ var target={
 	    // 用来产生一条待监听的路径
 		res = this; 
 		res.name = name;
+		if(!url.startsWith(this.root_url))
+			return null;
 		res.url = url; 
 		return res;
 	},
 };
 alltags = {}; //tag名为键，值为对应的target对象
 //撰写爬取程序
-alltags['中南大学新闻网']=target.newtag('中南大学新闻网',function(lasttime,callback){
+/*alltags['中南大学新闻网']=target.newtag('中南大学新闻网','http://news.csu.edu.cn/',function(lasttime,callback){
 	
-});
-alltags['中南大学计算机院']=target.newtag('中南大学计算机院',async function(lasttime,callback){
+});*/
+alltags['中南大学计算机院']=target.newtag('中南大学计算机院','https://cse.csu.edu.cn/',async function(lasttime,callback){
 	if(!check_url(this.url))
 		return;
-	await $.get(url,function(data){
-		dd=parseDom(data.responseText);
+	await $.get(this.url,function(data){
+		dd=parseDom(data);
 		remsg = [];
 		name = this.name;
 		dd.find('.download:first ul li').each(function(){
@@ -225,9 +252,11 @@ alltags['中南大学计算机院']=target.newtag('中南大学计算机院',asy
 			if(new Date(msg.time)>lasttime)
 				remsg.push(msg);
 		});
+		console.log(remsg.length);
 		callback(remsg);
 	});
 });
+NotifyServer.targets.push(alltags['中南大学计算机院'].create('通知公告','https://cse.csu.edu.cn/index/tzgg.htm'));
 
 //设置等待爬取链接
 function settargets(tarlist,callback)
@@ -246,6 +275,7 @@ function addtarget(tar, callback)
 {
 	NotifyServer.targets.push(alltags[tar.tag].create(tar.name,tar.url));
 	chrome.storage.sync.set({targets:NotifyServer.targets});
+	callback();
 }
 function deltarget(name,callback)
 {
@@ -253,6 +283,8 @@ function deltarget(name,callback)
     if(item.name === name) {
         NotifyServer.targets.splice(index, 1);
     }
+	chrome.storage.sync.set({targets:NotifyServer.targets});
+	callback();
 });
 }
 
@@ -263,7 +295,8 @@ function parseDom(arg) {
 　　 return $(objE);
 };
 
-/*部分测试
+//部分测试
+/*
 function clock()
 {
     var d=new Date();
@@ -271,9 +304,8 @@ function clock()
     notify("Clock work", t);
 }
 var work = setInterval(clock,3000);
-//test_items = [{title: 'test1', message: 'tttttttt'}, {title: 'test2', message: 'adaeafae'}, {title: 'test3', message: 'adxxxx'}];
-//notify_img('测试图片', '一段测试内容', icon);
-//notify_many("ayaya",test_items);
+test_items = [{title: 'test1', message: 'tttttttt'}, {title: 'test2', message: 'adaeafae'}, {title: 'test3', message: 'adxxxx'}];
+notify_img('测试图片', '一段测试内容', icon);
 */
 
 
@@ -286,5 +318,6 @@ function check_url(str){
 	else
 		return false;
 };
-var icon = 'img/icon.png';  //插件哦
+var icon = 'img/icon.png';  //插件图标
 NotifyServer.messages=[];  //储存通知
+NotifyServer.start(1000*10);
